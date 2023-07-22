e6bd4288f600fbe970fcc1134b790da56c936a3c
i-m-rishabh <uniquepandey125@gmail.com>
2023-07-22 22:44:16 +0530

    added signup form

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Signup from './pages/Signup';
+
+function App() {
+  return(
+    <Signup />
+  )  
+}
+
+export default App;
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+const Signup = () => {
+    
+// AIzaSyB9m55G39buJUFJOZ8yb8lJGhbC_7hTtyg
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    function handleEmailChange(event){
+        setErrorMessage('');
+        setSuccessMessage('');
+        const newEmail = event.target.value;
+        setEmail(newEmail);
+    }
+    function handlePasswordChange(event){
+        setErrorMessage('');
+        setSuccessMessage('');
+        const newPassword = event.target.value;
+        setPassword(newPassword);
+    }
+    function handleConfirmPasswordChange(event){
+        setErrorMessage('');
+        setSuccessMessage('');
+        const newConfirmPassword = event.target.value;
+        setConfirmPassword(newConfirmPassword);
+    }
+    // functin to handle user signup
+    async function handleUserSignup(){
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9m55G39buJUFJOZ8yb8lJGhbC_7hTtyg',{
+            method:'POST',
+            body: JSON.stringify({
+                email: email,
+                password: password,
+                returnSecureToken: true,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if(response.ok){
+            setSuccessMessage('congratulations! you have successfully signed up. Now login to continue');
+            // resetting input fields
+            setEmail('');
+            setPassword('')
+            setConfirmPassword('');
+        }else{
+            const data = await response.json();
+            setErrorMessage(data.error.message || 'error is not showing properly');
+        }
+
+    }
+    // function to handle form submit
+    function handleSignup(event){
+        event.preventDefault();
+        if(!email || !password || !confirmPassword){
+            setErrorMessage('ERROR! all fields are mendatory');
+            return;
+        }
+        if(password !== confirmPassword){
+            setErrorMessage('ERROR! password and confirm-password must be same');
+            return;
+        }
+        handleUserSignup();
+    }
+    
+    return(
+        <div className=" flex flex-col justify-center items-center w-screen h-screen">
+            <div className=" relative bg-primary py-10 px-10 rounded-xl flex flex-col justify-center items-center gap-5 text-white">
+                <h2 className=" text-xl mb-5">SignUp</h2>
+                {/* signup form */}
+                <form className="flex flex-col gap-3" onSubmit={handleSignup}>
+                    <div className="flex justify-between gap-1">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" onChange={handleEmailChange} value={email} className=" rounded text-black p-1" />
+                    </div>
+                    <div className="flex justify-between gap-1">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" onChange={handlePasswordChange} value={password} className=" rounded text-black p-1" />
+                    </div>
+                    <div className="flex justify-between gap-1">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" name="confirm-password" id="confirm-password" onChange={handleConfirmPasswordChange} value={confirmPassword} className=" rounded text-black p-1" />
+                    </div>
+                    <div className="flex justify-center mt-10">
+                        <button type="submit" className=" bg-secondary py-1 px-2 rounded text-black">sign up</button>
+                    </div>
+                </form>
+                {errorMessage && <p className=" text-red-300 ">{errorMessage}</p>}
+                {successMessage && <p className=" text-green-300">{successMessage}</p>}
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
